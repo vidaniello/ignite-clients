6748cbc861d966775867156832d46a7b4f7d6ce6
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-06-07 17:59:46 +0200

    Log4j2 test xml URL form gist.

--- src/test/java/com/github/vidaniello/ignite/Tests.java
+++ src/test/java/com/github/vidaniello/ignite/Tests.java
@@ -10,7 +10,7 @@ public class Tests {
 	static{
 
 	     //URL file di configurazione Log4j2
-	     System.setProperty("log4j.configurationFile", "http://address.tld/log4j2.xml");
+	     System.setProperty("log4j.configurationFile", "https://gist.github.com/vidaniello/e6cf911c5a440c05f57b946a15d27822/raw/c919f0b423d4b08ec18419cddc0b9bdb7c2e6599/log4j2-test-ignite.xml");
 
 	     //Tips per java.util.logging
 	     System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
