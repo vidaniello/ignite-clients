3807609a43a2f11a9af39369eb9460f9abbdd98c
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-06-07 17:52:25 +0200

    Util class.

---
+++ src/main/java/com/github/vidaniello/ignite/IgniteUtil.java
@@ -0,0 +1,24 @@
+package com.github.vidaniello.ignite;
+
+import java.net.URL;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.configuration.IgniteConfiguration;
+
+public class IgniteUtil {
+
+	private URL springConfigurationURL;
+	
+	private IgniteConfiguration conf;
+	
+	public IgniteUtil() {
+		
+	}
+	
+	public Ignite ignite() {
+		return null;
+	}
+	
+	
+	
+}
