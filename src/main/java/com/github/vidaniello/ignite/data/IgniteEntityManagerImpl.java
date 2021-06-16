0d185535e8c6bf3daafe63380fc744b85e213d38
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-06-16 17:48:31 +0200

    Improvements.

--- src/main/java/com/github/vidaniello/ignite/data/IgniteEntityManagerImpl.java
+++ src/main/java/com/github/vidaniello/ignite/data/IgniteEntityManagerImpl.java
@@ -1,5 +1,10 @@
 package com.github.vidaniello.ignite.data;
 
+import java.io.Serializable;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+
 public class IgniteEntityManagerImpl implements IgniteEntityManager {
 
 	/**
@@ -7,5 +12,19 @@ public class IgniteEntityManagerImpl implements IgniteEntityManager {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private Ignite ignite;
+	
+	
+	@SuppressWarnings("resource")
+	public <T extends Serializable> T create(T entity){
+		
+		IgniteCache<Serializable,T> cache = ignite.cache(entity.getClass().getCanonicalName());
+		
+		if(cache!=null) {
+			cache.put(entity, entity);
+		}
+		
+		return entity;
+	}
 	
 }
