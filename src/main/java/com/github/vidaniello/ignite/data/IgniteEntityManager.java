0ff0963312280328dd79e24717f290833823bd7b
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-11-19 10:50:00 +0100

    Added description.

--- src/main/java/com/github/vidaniello/ignite/data/IgniteEntityManager.java
+++ src/main/java/com/github/vidaniello/ignite/data/IgniteEntityManager.java
@@ -5,6 +5,11 @@ import java.util.Collection;
 
 import javax.cache.CacheException;
 
+/**
+ * Interface for entities
+ * @author Vincenzo D'Aniello (vidaniello@gmail.com) github.com/vidaniello
+ *
+ */
 public interface IgniteEntityManager extends Serializable {
 
 	public <T extends Serializable> T create(T entity) throws CacheException, Exception;
